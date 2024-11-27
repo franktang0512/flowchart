a03af999f3e1c3e2cc1005ff4f8c20a3f52de1f9
Frank Tang <frank051284@gmail.com>
2024-11-27 13:15:13 +0800

    selection

---
+++ js/113-w1/h1127.js
@@ -0,0 +1,57 @@
+var title = "遊樂園的票價是100元，但只要有學生證可以打8折，老人可以打65折，請做一個票價告知的機器，這台機器會讀取身分，然後告訴查詢的人所需要付的票價。";
+var question = "請做出成績判斷的流程！\n";
+var array = [
+    /*["夜市牛排", "200"],
+    ["藥燉排骨", "120"],
+    ["炸雞排", "60"],
+    ["奶茶", "55"],
+    ["臭豆腐", "40"],
+    ["地瓜球", "30"]*/
+]
+var test_case = [
+    {
+        input: "學生",
+        output: "80",
+        note: ""
+    },
+	{
+        input: "一般民眾",
+        output: "100",
+        note: ""
+    },
+	{
+        input: "老人",
+        output: "65",
+        note: ""
+    }
+]
+
+$(document).ready(function(){
+    $("#question_name").text("題目名稱："+title)
+    $("#question_info").text(question);
+    $("#example_testcase").append(
+        '<tr>\
+            <th style="width: 20%;">輸入</th>\
+            <th style="width: 20%;">輸出</th>\
+            <th style="width: 30%;">說明</th>\
+        </tr>'
+    )    
+    for(let i=0;i<test_case.length;i++){
+        $("#example_testcase").append(
+            '<tr>\
+                <td style="width: 20%;">'+test_case[i].input+'</td>\
+                <td style="width: 20%;">'+test_case[i].output+'</td>\
+                <td style="width: 30%;">'+test_case[i].note+'</td>\
+            </tr>'
+        )  
+    }
+    for(let i=0;i<array.length;i++){
+        $("#array").append(
+            '<tr>\
+                <td style="width: 20%;">'+array[i][0]+'</td>\
+                <td style="width: 20%;">'+array[i][1]+'</td>\
+            </tr>'
+        )  
+    }
+    
+})
